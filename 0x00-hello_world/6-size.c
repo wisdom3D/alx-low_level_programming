870749cc23fc5a21ef9dbee2f4f46d01a65f15d3
wisdom3D <wisdomkoudama@gmail.com>
2022-06-30 09:49:03 -0700

    Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of a char : %zu byte(s)\n", sizeof(char));
@@ -12,5 +13,6 @@ int main(void)
 	printf("Size of a long int : %zu byte(s)\n", sizeof(long int));
 	printf("Size of long long int : %zu byte(s)\n", sizeof(long long int ));
 	printf("Size of a float : %zu byte(s)\n", sizeof(float));
+
 	return (0);
 }
