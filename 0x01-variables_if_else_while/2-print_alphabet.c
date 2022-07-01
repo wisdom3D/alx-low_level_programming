aab3bb3c7daeaf9b7e26543731150891886df924
wisdom3D <85846460+wisdom3D@users.noreply.github.com>
2022-07-01 14:13:23 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char c;
+	
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
 	putchar('\n');
