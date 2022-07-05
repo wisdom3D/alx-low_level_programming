6994cbdd360088ddb330ff7441e8829307cfc552
wisdom3D <85846460+wisdom3D@users.noreply.github.com>
2022-07-05 19:39:52 +0000

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *_islower - checks for a lowercase character
- *@c: letter being tested
- * Return: Always 0
- */
+*_islower - checks for a lowercase character
+*@c: letter being tested
+* Return: Always 0
+*/
 
 int _islower(int c)
 {
