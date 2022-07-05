5a0687aa07b5db95b85559a74de68720e58968ec
wisdom3D <85846460+wisdom3D@users.noreply.github.com>
2022-07-05 22:22:02 +0000

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number 
- * @n: number being tested
- * Return: Always 0.
+ * print_last_digit - a function that prints the last digit of a number.
+ * @n: The parameter to be worked on.
+ *
+ * Return: Always n.
  */
-
 int print_last_digit(int n)
 {
-	n = n % 10;
-
+	n %= 10;
 	if (n < 0)
 		n = -n;
-		_putchar(n + '0');
-		return (0);
+
+	_putchar(n + '0');
+	return (n);
 }
