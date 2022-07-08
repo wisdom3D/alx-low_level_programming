cae42e8e6030e7ddc4444eaf7050c43b56787a50
wisdom3D <85846460+wisdom3D@users.noreply.github.com>
2022-07-08 00:26:40 +0000

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,12 +3,12 @@
 
 /**
  * print_line - Print line dependent on the integer n.
- * @n : The number of '-' characters to use
- * Return: void.
+ * @n : The number of '_' characters to use
+ * Return: Void.
  */
 void print_line(int n)
 {
-	int i; 
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
