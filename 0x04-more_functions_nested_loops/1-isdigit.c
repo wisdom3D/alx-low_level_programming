661684204724a419a84cd4b1756d6ffc3d794d74
wisdom3D <85846460+wisdom3D@users.noreply.github.com>
2022-07-08 00:15:04 +0000

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * _isdigit - Check if parameter c represent an digit.
+ * _isdigit - Check if paramter c represents an digit.
  * @c: Int representing an ascii character
  * Return: Return 1 if it is a digit otherwise return 0.
  */
 int _isdigit(int c)
 {
-	if (c >= 40 && c <= 57)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
