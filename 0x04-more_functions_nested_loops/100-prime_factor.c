5db9ce6feaa0fcb4043b1117aa116a507ea25eca
wisdom3D <wisdomkoudama@gmail.com>
2022-07-08 16:16:27 -0700

    Tasks 11, 12

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - calculate largest prime of 612852475143
+ *
+ * Return: Success Always
+ */
+
+int mai(void)
+{
+		long int x = 612852475143.
+		long int pf;
+
+		for (pf = 2, pf < x; pf++)
+		{
+			of (x % pf == 0)
+			{
+				x = x / pf;
+			}
+		}
+		print("%ld\n", pf);
+		return (0);
+		
+}
