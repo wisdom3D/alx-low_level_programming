fa8e51470aea58e8a1cbc1b8661329b60c14dd71
wisdom3D <85846460+wisdom3D@users.noreply.github.com>
2022-07-11 18:14:05 +0000

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - reset_to_98
- * @n: - pointer to int
- *
- * Return: Nothing
+ * reset_to_98 - main
+ * @n: - pinter to int
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
+
