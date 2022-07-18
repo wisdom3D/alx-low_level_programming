9e707af9a24678a89adca5836d73d9f48d18e97d
wisdom3D <85846460+wisdom3D@users.noreply.github.com>
2022-07-18 22:20:30 +0000

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,22 +2,19 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints sums
+ * print_diagsums -  prints sums
  * @a: a pointer
  * @size: size
  */
-
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int sum1 = 0;
-	int sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		sum1 += *(a + (size * i + i));
 		sum2 += *(a + (size * i + size - 1 - i));
 	}
-	printf("%d, ",sum1);
+	printf("%d, ", sum1);
 	printf("%d\n", sum2);
 }
